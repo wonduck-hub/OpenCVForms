83c040f7d070a4e93faff17ab9cfbff5f3e02992
wonduck-hub <dnjsejr417@naver.com>
2024-10-14 14:44:09 +0900

    Add project files.

---
+++ OpenCVForms/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace OpenCVForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBoxImage = new PictureBox();
+            LoadBtn = new Button();
+            RevertButton = new Button();
+            GrayButton = new Button();
+            DilationButton = new Button();
+            ErosionButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxImage).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBoxImage
+            // 
+            pictureBoxImage.Location = new Point(24, 19);
+            pictureBoxImage.Name = "pictureBoxImage";
+            pictureBoxImage.Size = new Size(667, 361);
+            pictureBoxImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxImage.TabIndex = 0;
+            pictureBoxImage.TabStop = false;
+            // 
+            // LoadBtn
+            // 
+            LoadBtn.Location = new Point(1254, 19);
+            LoadBtn.Name = "LoadBtn";
+            LoadBtn.Size = new Size(112, 34);
+            LoadBtn.TabIndex = 1;
+            LoadBtn.Text = "load btn";
+            LoadBtn.UseVisualStyleBackColor = true;
+            LoadBtn.Click += LoadBtn_Click;
+            // 
+            // RevertButton
+            // 
+            RevertButton.Location = new Point(1254, 59);
+            RevertButton.Name = "RevertButton";
+            RevertButton.Size = new Size(112, 34);
+            RevertButton.TabIndex = 2;
+            RevertButton.Text = "revert";
+            RevertButton.UseVisualStyleBackColor = true;
+            RevertButton.Click += RevertButton_Click;
+            // 
+            // GrayButton
+            // 
+            GrayButton.Location = new Point(1254, 99);
+            GrayButton.Name = "GrayButton";
+            GrayButton.Size = new Size(112, 34);
+            GrayButton.TabIndex = 3;
+            GrayButton.Text = "gray";
+            GrayButton.UseVisualStyleBackColor = true;
+            GrayButton.Click += SetGrayButton_Click;
+            // 
+            // DilationButton
+            // 
+            DilationButton.Location = new Point(1254, 225);
+            DilationButton.Name = "DilationButton";
+            DilationButton.Size = new Size(112, 34);
+            DilationButton.TabIndex = 4;
+            DilationButton.Text = "dilation";
+            DilationButton.UseVisualStyleBackColor = true;
+            DilationButton.Click += DilationButton_Click;
+            // 
+            // ErosionButton
+            // 
+            ErosionButton.Location = new Point(1254, 265);
+            ErosionButton.Name = "ErosionButton";
+            ErosionButton.Size = new Size(112, 34);
+            ErosionButton.TabIndex = 5;
+            ErosionButton.Text = "erosion";
+            ErosionButton.UseVisualStyleBackColor = true;
+            ErosionButton.Click += ErosionButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1378, 467);
+            Controls.Add(ErosionButton);
+            Controls.Add(DilationButton);
+            Controls.Add(GrayButton);
+            Controls.Add(RevertButton);
+            Controls.Add(LoadBtn);
+            Controls.Add(pictureBoxImage);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBoxImage).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBoxImage;
+        private Button LoadBtn;
+        private Button RevertButton;
+        private Button GrayButton;
+        private Button DilationButton;
+        private Button ErosionButton;
+    }
+}
---
+++ OpenCVForms/Form1.cs
@@ -0,0 +1,110 @@
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System.Diagnostics;
+namespace OpenCVForms
+{
+    public partial class Form1 : Form
+    {
+        Mat image = new Mat();
+        public Form1()
+        {
+            InitializeComponent();
+
+            DisableButton();
+
+        }
+
+        private void DisableButton()
+        {
+            RevertButton.Enabled = false;
+            GrayButton.Enabled = false;
+            DilationButton.Enabled = false;
+            ErosionButton.Enabled = false;
+        }
+
+        private void ActivateButton()
+        {
+            RevertButton.Enabled = true;
+            GrayButton.Enabled = true;
+            DilationButton.Enabled = true;
+            ErosionButton.Enabled = true;
+        }
+
+        #region 파일 로드, 원본 표시, 회색조
+        private void LoadBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    image = Cv2.ImRead(dlg.FileName);
+
+                    pictureBoxImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image);
+
+                    ActivateButton();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void RevertButton_Click(object sender, EventArgs e)
+        {
+            Debug.Assert(image != null);
+
+            pictureBoxImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image);
+
+        }
+
+        private void SetGrayButton_Click(object sender, EventArgs e)
+        {
+            Debug.Assert(image != null);
+
+            Mat grayImage = new Mat();
+            Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGR2GRAY);
+
+            pictureBoxImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(grayImage);
+        }
+        #endregion
+
+        #region 모폴로지 변환
+        private void DilationButton_Click(object sender, EventArgs e)
+        {
+            Debug.Assert(image != null);
+
+            Mat grayImage = new Mat();
+            Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGR2GRAY);
+
+            Mat dilationImage = new Mat();
+            Mat kernel = Cv2.GetStructuringElement(MorphShapes.Cross, new OpenCvSharp.Size(7, 7));
+
+            Cv2.Dilate(grayImage, dilationImage, kernel);
+
+            pictureBoxImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(dilationImage);
+        }
+
+        private void ErosionButton_Click(object sender, EventArgs e)
+        {
+            Debug.Assert(image != null);
+
+            Mat grayImage = new Mat();
+            Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGR2GRAY);
+
+            Mat ErosoinImage = new Mat();
+            Mat kernel = Cv2.GetStructuringElement(MorphShapes.Cross, new OpenCvSharp.Size(7, 7));
+
+            Cv2.Erode(grayImage, ErosoinImage, kernel);
+
+            pictureBoxImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(ErosoinImage);
+
+        }
+        #endregion
+
+        #region 모폴로지 연산
+
+        #endregion
+    }
+}
