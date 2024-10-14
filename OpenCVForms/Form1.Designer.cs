8d60959153e9a39699c1baaae5437044069391a1
장원덕 <dnjsejr417@outlook.com>
2024-10-14 22:14:52 +0900

    모폴로지 연산 예제 추가

--- OpenCVForms/Form1.Designer.cs
+++ OpenCVForms/Form1.Designer.cs
@@ -34,23 +34,31 @@
             GrayButton = new Button();
             DilationButton = new Button();
             ErosionButton = new Button();
+            OpenButton = new Button();
+            CloseButton = new Button();
+            GradientButton = new Button();
+            TopHatButton = new Button();
+            BlackHatButton = new Button();
+            HitMissButton = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBoxImage).BeginInit();
             SuspendLayout();
             // 
             // pictureBoxImage
             // 
-            pictureBoxImage.Location = new Point(24, 19);
+            pictureBoxImage.Location = new Point(17, 11);
+            pictureBoxImage.Margin = new Padding(2);
             pictureBoxImage.Name = "pictureBoxImage";
-            pictureBoxImage.Size = new Size(667, 361);
+            pictureBoxImage.Size = new Size(600, 342);
             pictureBoxImage.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBoxImage.TabIndex = 0;
             pictureBoxImage.TabStop = false;
             // 
             // LoadBtn
             // 
-            LoadBtn.Location = new Point(1254, 19);
+            LoadBtn.Location = new Point(878, 11);
+            LoadBtn.Margin = new Padding(2);
             LoadBtn.Name = "LoadBtn";
-            LoadBtn.Size = new Size(112, 34);
+            LoadBtn.Size = new Size(78, 20);
             LoadBtn.TabIndex = 1;
             LoadBtn.Text = "load btn";
             LoadBtn.UseVisualStyleBackColor = true;
@@ -58,9 +66,10 @@
             // 
             // RevertButton
             // 
-            RevertButton.Location = new Point(1254, 59);
+            RevertButton.Location = new Point(878, 35);
+            RevertButton.Margin = new Padding(2);
             RevertButton.Name = "RevertButton";
-            RevertButton.Size = new Size(112, 34);
+            RevertButton.Size = new Size(78, 20);
             RevertButton.TabIndex = 2;
             RevertButton.Text = "revert";
             RevertButton.UseVisualStyleBackColor = true;
@@ -68,9 +77,10 @@
             // 
             // GrayButton
             // 
-            GrayButton.Location = new Point(1254, 99);
+            GrayButton.Location = new Point(878, 59);
+            GrayButton.Margin = new Padding(2);
             GrayButton.Name = "GrayButton";
-            GrayButton.Size = new Size(112, 34);
+            GrayButton.Size = new Size(78, 20);
             GrayButton.TabIndex = 3;
             GrayButton.Text = "gray";
             GrayButton.UseVisualStyleBackColor = true;
@@ -78,9 +88,10 @@
             // 
             // DilationButton
             // 
-            DilationButton.Location = new Point(1254, 225);
+            DilationButton.Location = new Point(878, 135);
+            DilationButton.Margin = new Padding(2);
             DilationButton.Name = "DilationButton";
-            DilationButton.Size = new Size(112, 34);
+            DilationButton.Size = new Size(78, 20);
             DilationButton.TabIndex = 4;
             DilationButton.Text = "dilation";
             DilationButton.UseVisualStyleBackColor = true;
@@ -88,25 +99,99 @@
             // 
             // ErosionButton
             // 
-            ErosionButton.Location = new Point(1254, 265);
+            ErosionButton.Location = new Point(878, 159);
+            ErosionButton.Margin = new Padding(2);
             ErosionButton.Name = "ErosionButton";
-            ErosionButton.Size = new Size(112, 34);
+            ErosionButton.Size = new Size(78, 20);
             ErosionButton.TabIndex = 5;
             ErosionButton.Text = "erosion";
             ErosionButton.UseVisualStyleBackColor = true;
             ErosionButton.Click += ErosionButton_Click;
             // 
+            // OpenButton
+            // 
+            OpenButton.Location = new Point(779, 135);
+            OpenButton.Margin = new Padding(2);
+            OpenButton.Name = "OpenButton";
+            OpenButton.Size = new Size(78, 20);
+            OpenButton.TabIndex = 6;
+            OpenButton.Text = "opening";
+            OpenButton.UseVisualStyleBackColor = true;
+            OpenButton.Click += OpenButton_Click;
+            // 
+            // CloseButton
+            // 
+            CloseButton.Location = new Point(779, 159);
+            CloseButton.Margin = new Padding(2);
+            CloseButton.Name = "CloseButton";
+            CloseButton.Size = new Size(78, 20);
+            CloseButton.TabIndex = 7;
+            CloseButton.Text = "closing";
+            CloseButton.UseVisualStyleBackColor = true;
+            CloseButton.Click += CloseButton_Click;
+            // 
+            // GradientButton
+            // 
+            GradientButton.Location = new Point(779, 183);
+            GradientButton.Margin = new Padding(2);
+            GradientButton.Name = "GradientButton";
+            GradientButton.Size = new Size(78, 20);
+            GradientButton.TabIndex = 8;
+            GradientButton.Text = "gradient";
+            GradientButton.UseVisualStyleBackColor = true;
+            GradientButton.Click += GradientButton_Click;
+            // 
+            // TopHatButton
+            // 
+            TopHatButton.Location = new Point(697, 135);
+            TopHatButton.Margin = new Padding(2);
+            TopHatButton.Name = "TopHatButton";
+            TopHatButton.Size = new Size(78, 20);
+            TopHatButton.TabIndex = 9;
+            TopHatButton.Text = "TopHat";
+            TopHatButton.UseVisualStyleBackColor = true;
+            TopHatButton.Click += TopHatButton_Click;
+            // 
+            // BlackHatButton
+            // 
+            BlackHatButton.Location = new Point(697, 159);
+            BlackHatButton.Margin = new Padding(2);
+            BlackHatButton.Name = "BlackHatButton";
+            BlackHatButton.Size = new Size(78, 20);
+            BlackHatButton.TabIndex = 10;
+            BlackHatButton.Text = "BlackHat";
+            BlackHatButton.UseVisualStyleBackColor = true;
+            BlackHatButton.Click += BlackHatButton_Click;
+            // 
+            // HitMissButton
+            // 
+            HitMissButton.Location = new Point(697, 183);
+            HitMissButton.Margin = new Padding(2);
+            HitMissButton.Name = "HitMissButton";
+            HitMissButton.Size = new Size(78, 20);
+            HitMissButton.TabIndex = 11;
+            HitMissButton.Text = "HitMiss";
+            HitMissButton.UseVisualStyleBackColor = true;
+            HitMissButton.Click += HitMissButton_Click;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1378, 467);
+            ClientSize = new Size(965, 434);
+            Controls.Add(HitMissButton);
+            Controls.Add(BlackHatButton);
+            Controls.Add(TopHatButton);
+            Controls.Add(GradientButton);
+            Controls.Add(CloseButton);
+            Controls.Add(OpenButton);
             Controls.Add(ErosionButton);
             Controls.Add(DilationButton);
             Controls.Add(GrayButton);
             Controls.Add(RevertButton);
             Controls.Add(LoadBtn);
             Controls.Add(pictureBoxImage);
+            Margin = new Padding(2);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBoxImage).EndInit();
@@ -121,5 +206,11 @@
         private Button GrayButton;
         private Button DilationButton;
         private Button ErosionButton;
+        private Button OpenButton;
+        private Button CloseButton;
+        private Button GradientButton;
+        private Button TopHatButton;
+        private Button BlackHatButton;
+        private Button HitMissButton;
     }
 }
--- OpenCVForms/Form1.cs
+++ OpenCVForms/Form1.cs
@@ -20,6 +20,12 @@ namespace OpenCVForms
             GrayButton.Enabled = false;
             DilationButton.Enabled = false;
             ErosionButton.Enabled = false;
+            OpenButton.Enabled = false;
+            CloseButton.Enabled = false;
+            GradientButton.Enabled = false;
+            TopHatButton.Enabled = false;
+            BlackHatButton.Enabled = false;
+            HitMissButton.Enabled = false;
         }
 
         private void ActivateButton()
@@ -28,6 +34,12 @@ namespace OpenCVForms
             GrayButton.Enabled = true;
             DilationButton.Enabled = true;
             ErosionButton.Enabled = true;
+            OpenButton.Enabled = true;
+            CloseButton.Enabled = true;
+            GradientButton.Enabled = true;
+            TopHatButton.Enabled = true;
+            BlackHatButton.Enabled = true;
+            HitMissButton.Enabled = true;
         }
 
         #region 파일 로드, 원본 표시, 회색조
@@ -104,7 +116,103 @@ namespace OpenCVForms
         #endregion
 
         #region 모폴로지 연산
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            Debug.Assert(image != null);
 
+            Mat grayImage = new Mat();
+            Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGR2GRAY);
+
+            Mat openImage = new Mat();
+            Mat kernel = Cv2.GetStructuringElement(MorphShapes.Cross, new OpenCvSharp.Size(7, 7));
+
+            Cv2.MorphologyEx(grayImage, openImage, MorphTypes.Open, kernel);
+
+            pictureBoxImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(openImage);
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Debug.Assert(image != null);
+
+            Mat grayImage = new Mat();
+            Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGR2GRAY);
+
+            Mat closeImage = new Mat();
+            Mat kernel = Cv2.GetStructuringElement(MorphShapes.Cross, new OpenCvSharp.Size(7, 7));
+
+            Cv2.MorphologyEx(grayImage, closeImage, MorphTypes.Close, kernel);
+
+            pictureBoxImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(closeImage);
+        }
+
+        private void GradientButton_Click(object sender, EventArgs e)
+        {
+            Debug.Assert(image != null);
+
+            Mat grayImage = new Mat();
+            Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGR2GRAY);
+
+            Mat closeImage = new Mat();
+            Mat kernel = Cv2.GetStructuringElement(MorphShapes.Cross, new OpenCvSharp.Size(7, 7));
+
+            Cv2.MorphologyEx(grayImage, closeImage, MorphTypes.Gradient, kernel);
+
+            pictureBoxImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(closeImage);
+        }
+
+        private void TopHatButton_Click(object sender, EventArgs e)
+        {
+            Debug.Assert(image != null);
+
+            Mat grayImage = new Mat();
+            Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGR2GRAY);
+
+            Mat closeImage = new Mat();
+            Mat kernel = Cv2.GetStructuringElement(MorphShapes.Cross, new OpenCvSharp.Size(7, 7));
+
+            Cv2.MorphologyEx(grayImage, closeImage, MorphTypes.TopHat, kernel);
+
+            pictureBoxImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(closeImage);
+        }
+
+        private void BlackHatButton_Click(object sender, EventArgs e)
+        {
+            Debug.Assert(image != null);
+
+            Mat grayImage = new Mat();
+            Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGR2GRAY);
+
+            Mat closeImage = new Mat();
+            Mat kernel = Cv2.GetStructuringElement(MorphShapes.Cross, new OpenCvSharp.Size(7, 7));
+
+            Cv2.MorphologyEx(grayImage, closeImage, MorphTypes.BlackHat, kernel);
+
+            pictureBoxImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(closeImage);
+        }
+
+        private void HitMissButton_Click(object sender, EventArgs e)
+        {
+            Debug.Assert(image != null);
+
+            Mat grayImage = new Mat();
+            Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGR2GRAY);
+
+            Mat closeImage = new Mat();
+
+            // 커널 메트릭스에서 0은 무시, 1은 유지를 한다.
+            Mat kernel = Mat.Zeros(new OpenCvSharp.Size(7, 7), MatType.CV_8U);
+            kernel[0, 7, 0, 1] = Mat.Ones(new OpenCvSharp.Size(1, 7), MatType.CV_8U);
+            kernel[0, 1, 0, 7] = Mat.Ones(new OpenCvSharp.Size(7, 1), MatType.CV_8U);
+
+            Cv2.MorphologyEx(grayImage, closeImage, MorphTypes.HitMiss, kernel, iterations:10);
+
+            pictureBoxImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(closeImage);
+        }
         #endregion
+
+
+
+
     }
 }
